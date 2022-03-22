268f40eeec83a894eab2d55aabb9603810671c0d
menna refaat <menna.ref3t@gmail.com>
2022-03-22 14:58:20 +0200

    03-suites-test-cases

---
+++ cypress/integration/03-suites-test-cases.spec.ts
@@ -0,0 +1,40 @@
+
+describe('My First Test', () => {
+  
+   it('test log command ', () => {
+    
+   })
+
+    it('test visit command to localhost ', () => {
+    
+    })
+
+    it('test visit command to any web site', () => {
+      
+    })
+
+
+    it('test visit command to localhost, and display a view port ', () => {
+    
+
+    })
+
+    it('test visit command to localhost and display view port2', () => {
+     
+
+    })
+
+    //testing => getting element + making actions
+    
+    it('test visit command to localhost and get element ', () => {
+   
+
+    })
+
+    beforeEach(("visit url before every test case"),() =>{
+       
+    })
+
+    
+  })
+  
